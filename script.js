8f61c7237e5b689b8d83ca553d1385805bdd1ba4
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-04-30 21:02:57 +0200

    for loop test

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
+{
+let h = document.createElement('h1'); 
+let headerText = document.createTextNode('Det her er en header');
+h.appendChild(headerText);
+document.querySelector('h1').appendChild(h);
+}
 
-let h = document.createElement('h1')
-let headerText = document.createTextNode('Det her er en header')
-h.appendChild(headerText)
-document.querySelector('h1').appendChild(h)
+const main = document.querySelector('main');
+
+let shopBox = "";
+
+for ( let i = 0; i < 10; i++) {
+shopBox += '<div>køb</div>';
+}
+
+main.innerHTML = shopBox;
